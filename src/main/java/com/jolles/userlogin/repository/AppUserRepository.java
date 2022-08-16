3d5d8c5c79da8344dbdfeb8e5fb9b2bd2e413022
Ethan Jolles <erjolles@gmail.com>
2022-08-16 08:40:06 -0400

    Implement AppUserRepository

---
+++ src/main/java/com/jolles/userlogin/repository/AppUserRepository.java
@@ -0,0 +1,13 @@
+package com.jolles.userlogin.repository;
+
+import com.jolles.userlogin.model.AppUser;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+    Optional<AppUser> findByEmail(String email);
+}
